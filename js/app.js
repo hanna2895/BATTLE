ccab7b7bf13b5ce35ff280d28a95f3a2caac8d98
Hannah Werman <hannah.werman@gmail.com>
2018-03-30 11:48:04 -0500

    added canvas and displayed battle text on start page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log("hi");
+
+const canvas = document.getElementById('my-canvas');
+
+// this is something you will basically always need for 2d canvas art
+const ctx = canvas.getContext('2d');
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+ctx.beginPath();
+ctx.font = '48px serif';
+ctx.textAlign ='center';
+ctx.fillStyle = 'blue';
+ctx.fillText('BATTLE', x, y - 100);
