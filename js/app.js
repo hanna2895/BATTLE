aad65a1ba9482e4651f5d99a956de4210de15f6c
Hannah Werman <hannah.werman@gmail.com>
2018-03-30 12:20:28 -0500

    added start button to clear canvas and move to next level

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@ console.log("hi");
 
 const canvas = document.getElementById('my-canvas');
 
-// this is something you will basically always need for 2d canvas art
 const ctx = canvas.getContext('2d');
+
+// to center stuff on the canvas
 const x = canvas.width / 2;
 const y = canvas.height / 2;
+
+// make the Battle text on the home screen
 ctx.beginPath();
 ctx.font = '48px serif';
 ctx.textAlign ='center';
 ctx.fillStyle = 'blue';
 ctx.fillText('BATTLE', x, y - 100);
+
+// button to start the game
+$('#start').on('click', () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
